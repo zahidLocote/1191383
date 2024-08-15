2e017e5a93470705027648a52663bf5b58b6b238
Diego Carballo <carballo.diego@uabc.edu.mx>
2024-08-14 19:25:02 -0700

    ejercicio11

---
+++ ejercicio11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
